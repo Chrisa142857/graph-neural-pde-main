c83fc8ba6efd38c744ac330f10e55325eab1e5d4
Ziquan Wei <ziquanw@theia.ia.unc.edu>
2023-04-30 11:59:22 -0400

    Initial GRAND

---
+++ src/cora.sh
@@ -0,0 +1,7 @@
+python run_GNN.py --dataset 'Cora' --planetoid_split --time 2.0 --gpu 1
+python run_GNN.py --dataset 'Cora' --planetoid_split --time 4.0 --gpu 1
+python run_GNN.py --dataset 'Cora' --planetoid_split --time 8.0 --gpu 1
+python run_GNN.py --dataset 'Cora' --planetoid_split --time 16.0 --gpu 1
+python run_GNN.py --dataset 'Cora' --planetoid_split --time 32.0 --gpu 1
+python run_GNN.py --dataset 'Cora' --planetoid_split --time 64.0 --gpu 1
+python run_GNN.py --dataset 'Cora' --planetoid_split --time 128.0 --gpu 1
---
+++ src/pubmed.sh
@@ -0,0 +1,7 @@
+python run_GNN.py --dataset 'Pubmed' --planetoid_split --time 2.0 --epoch 200 --gpu 3
+python run_GNN.py --dataset 'Pubmed' --planetoid_split --time 4.0 --epoch 200 --gpu 3
+python run_GNN.py --dataset 'Pubmed' --planetoid_split --time 8.0 --epoch 200 --gpu 3
+python run_GNN.py --dataset 'Pubmed' --planetoid_split --time 16.0 --epoch 200 --gpu 3
+python run_GNN.py --dataset 'Pubmed' --planetoid_split --time 32.0 --epoch 200 --gpu 3
+python run_GNN.py --dataset 'Pubmed' --planetoid_split --time 64.0 --epoch 200 --gpu 3
+python run_GNN.py --dataset 'Pubmed' --planetoid_split --time 128.0 --epoch 200 --gpu 3
